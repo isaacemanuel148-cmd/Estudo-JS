713c70aee1ad68ea3e7525611a146bad7185c5aa
Isac Emanuel <isaacemanuel148@gmail.com>
2026-02-06 19:55:16 -0300

    novo arquivo de exercicio

---
+++ exercicios/Exerci_1.js
@@ -0,0 +1,5 @@
+// 1.Variáveis e tipos de dados: Escreva um programa que declare duas variáveis, “nome” e “idade”, e as imprima em um console em uma frase que diga “Olá, meu nome é [nome] e eu tenho [idade] anos”.
+let nome = "Isac";
+let idade = 19;
+
+console.log("Ola, meu nome é" + nome + "e tenho" + idade + "anos");
