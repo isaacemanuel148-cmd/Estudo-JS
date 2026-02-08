a22b9ca294c7396b0095a8b44d3424f716546fda
Isac Emanuel <isaacemanuel148@gmail.com>
2026-02-08 14:12:53 -0300

    mexi no exerci_2

--- exercicios/Exerci_2.js
+++ exercicios/Exerci_2.js
@@ -5,3 +5,7 @@ let soma = num1 + num2;
 let subtracao = num1 - num2;
 let multiplicacao = num1 * num2;
 let divisao = num1 / num2;  
+console.log("A soma é: " + soma);
+console.log("A subtração é: " + subtracao);
+console.log("A multiplicação é: " + multiplicacao);
+console.log("A divisão é: " + divisao);
