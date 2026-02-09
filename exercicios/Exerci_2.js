624906fcd1a264aba87920d4b532d7a49bc0db7b
Isac Emanuel <isaacemanuel148@gmail.com>
2026-02-08 22:46:59 -0300

    alterção no arquivo Erxerci_2

--- exercicios/Exerci_2.js
+++ exercicios/Exerci_2.js
@@ -1,1 +1,7 @@
 //Crie um programa que solicite ao usuário dois números e exiba o resultado da soma, subtração, multiplicação e divisão desses números.
+let numero = 2
+let numero2 = 5
+
+let soma = numero + numero2
+
+console.log("A soma dos dois números é: " + soma)
