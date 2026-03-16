5e820577dbbf9ea93c2b3e3e13cac25e68f8d99b
Isac Emanuel <isaacemanuel148@gmail.com>
2026-03-16 19:51:20 -0300

    novos conteudo e nova pasta dos conteudos de controle de js

--- Fundamentos/inicio/errro.js
+++ Fundamentos/inicio/errro.js
@@ -1,7 +1,31 @@
+const prompt = require('prompt-sync')()
+
 function tratarErroELanacar(erro){
-    throw 
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        data: new Date
+    }
 }
 
 function imprimiNomeGritado(obj){
-    
+    try{
+        console.log(obj.nome.toUpperCase() + "!!!")
+        console.log(typeof obj.nome)
+        
+    }
+    catch(e){
+        tratarErroELanacar(e)     
+    }
+    finally{
+        console.log("Sucesso!!")
+    }
 }
+
+const nome = prompt('Digite seu nome:')
+
+const obj = {
+    nome: nome
+}
+imprimiNomeGritado(obj)
+
